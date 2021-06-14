3e492edd2d689dcd2db19bcd93585a5cc7bbdedc
Sergioarg <sergio_ramos53@yahoo.com>
2021-06-14 14:43:22 -0500

    :tada: Add README.md, 2-selection_sort.c update header

---
+++ 2-selection_sort.c
@@ -0,0 +1,35 @@
+#include "sort.h"
+/**
+ * swap_value - swap the values of the array
+ *
+ * @x_value: x value of the array
+ * @y_value: y value of the array
+ */
+void swap_value(int *x_value, int *y_value)
+{
+	int temp = *x_value;
+	*x_value = *y_value;
+	*y_value = temp;
+}
+
+/**
+ * selection_sort - algorithm that sorts
+ * an array of integers in ascending order
+ *
+ * @array: array to sort
+ * @size: size of the array to sort
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, min_idx;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min_idx = i;
+		for (j = i + 1; j < size; j++)
+			if (array[j] < array[min_idx])
+				min_idx = j;
+		swap_value(&array[min_idx], &array[i]);
+		print_array(array, size);
+	}
+}
--- sort.h
+++ sort.h
@@ -22,6 +22,8 @@ listint_t *create_listint(const int *array, size_t size);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
+
+/* Includes prototipes functions */
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
